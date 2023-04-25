a928ba880561fb54e90518e35c784513500a77d1
Thomas Booth <thomasbooth26@gmail.com>
2023-04-25 23:34:44 +0100

    Modal Setup

--- property-site/app/layout.tsx
+++ property-site/app/layout.tsx
@@ -1,3 +1,5 @@
+import ClientOnly from './components/ClientOnly'
+import RegisterModal from './components/modals/RegisterModal'
 import Navbar from './components/navbar/Navbar'
 import './globals.css'
 import { Nunito } from 'next/font/google'
@@ -18,7 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className = {font.className}>
-        <Navbar />
+        {/* we use client only here to stop hydration errors (bug) */}
+        <ClientOnly>
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
           {children}
       </body>
     </html>
