a931f3fd2d8f49c5090b9ef1acc1aff3baa90867
Thomas Booth <thomasbooth26@gmail.com>
2023-04-27 00:25:54 +0100

    Authentication finished

--- property-site/app/components/navbar/UserMenu.tsx
+++ property-site/app/components/navbar/UserMenu.tsx
@@ -6,11 +6,11 @@ import { useCallback, useState } from 'react'
 import MenuItem from './MenuItem'
 import useRegisterModal from '@/app/hooks/useRegisterModal'
 import useLoginModal from '@/app/hooks/useLoginModal'
-import { User } from '@prisma/client'
 import { signOut } from 'next-auth/react'
+import { SafeUser } from '@/app/types'
 
 interface UserMenuProps {
-    currentUser?: User
+    currentUser?: SafeUser | null
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({
@@ -35,7 +35,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
             className = 'p-4 md:py-1 md:px-2 border-[1px] border-neutral-200 flex flex-row items-center gap-3 rounded-full cursor-pointer hover:shadow-md transition'>
                 <AiOutlineMenu />
                 <div className = 'hidden md:block'>
-                    <Avatar />
+                    <Avatar src = {currentUser?.image} />
                 </div>
             </div>
         </div>
--- property-site/app/layout.tsx
+++ property-site/app/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
           <ToasterProvider />
           <LoginModal />
           <RegisterModal />
-          <Navbar currentUser = {currentUser} />
+          <Navbar currentUser = {currentUser}/>
         </ClientOnly>
           {children}
       </body>
