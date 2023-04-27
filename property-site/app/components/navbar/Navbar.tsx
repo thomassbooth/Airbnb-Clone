025671689bf150343f271b5dba9aa1e44cefeb09
Thomas Booth <thomasbooth26@gmail.com>
2023-04-27 22:19:05 +0100

    Categories on navbar

--- property-site/app/components/navbar/Navbar.tsx
+++ property-site/app/components/navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import Container from "../Container"
+import Categories from "./Categories"
 import { Logo } from "./Logo"
 import Search from "./Search"
 import UserMenu from "./UserMenu"
@@ -23,6 +24,7 @@ const Navbar: React.FC<NavbarProps> = ({
                 </div>
             </Container>
         </div>
+        <Categories />
     </div>
   )
 }
