4f1a307592a9143d86efc9ad7ad8f2f08fadc44e
Thomas Booth <thomasbooth26@gmail.com>
2023-04-30 23:03:23 +0100

    Empty filters state

--- property-site/app/page.tsx
+++ property-site/app/page.tsx
@@ -1,12 +1,32 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import ClientOnly from './components/ClientOnly'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const isEmpty = true;
+
+  if(isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState 
+          showReset
+        />
+      </ClientOnly>
+    )
+  }
+
+
   return (
-    <div>
-      
-    </div>
+    <ClientOnly>
+      <Container>
+        <div className = 'pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+          <div>My future listings</div>
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
