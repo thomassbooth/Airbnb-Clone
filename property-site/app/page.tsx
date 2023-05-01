f340855a76105e1288b0fe08a2145578e1d2823b
Thomas Booth <thomasbooth26@gmail.com>
2023-05-01 20:38:31 +0100

    Listings on homepage displayed

--- property-site/app/page.tsx
+++ property-site/app/page.tsx
@@ -3,13 +3,17 @@ import { Inter } from 'next/font/google'
 import ClientOnly from './components/ClientOnly'
 import Container from './components/Container'
 import EmptyState from './components/EmptyState'
+import getListings from './actions/getListings'
+import ListingCard from './components/listings/ListingCard'
+import getCurrentUser from './actions/getCurrentUser'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  const isEmpty = true;
+export default async function Home() {
+  const listings = await getListings()
+  const currentUser = await getCurrentUser()
 
-  if(isEmpty) {
+  if(listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState 
@@ -24,7 +28,15 @@ export default function Home() {
     <ClientOnly>
       <Container>
         <div className = 'pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-          <div>My future listings</div>
+          {listings.map((listing: any) => {
+            return (
+              <ListingCard
+                currentUser = {currentUser}
+                key = {listing.id}
+                data = {listing}
+              />
+            )
+          })}
         </div>
       </Container>
     </ClientOnly>
